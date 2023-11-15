2554e8639de34be8e2ef794db2469b7132e438e9
Dharshini <104815447+dharshu2323@users.noreply.github.com>
2023-11-15 16:05:30 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import React from 'react';
+import Home from "./pages/Home";
+import Navbar from "./layout/NavBar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AddUser from "./user/AddUser";
+import EditUser from "./user/EditUser";
+import ViewUser from "./user/ViewUser";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/adduser" element={<AddUser />} />
+          <Route exact path="/edituser/:id" element={<EditUser />} />
+          <Route exact path="/viewuser/:id" element={<ViewUser />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
